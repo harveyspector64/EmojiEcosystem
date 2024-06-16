07a1736af5a3bf5058374587992a9f0ce4cf2cd5
harveyspector64 <154754949+harveyspector64@users.noreply.github.com>
2024-06-16 12:49:24 -0700

    Update floweringBush.js

--- floweringBush.js
+++ floweringBush.js
@@ -2,21 +2,26 @@ class FloweringBush {
     constructor(x, y) {
         this.x = x;
         this.y = y;
-        this.element = document.createElement('img');
-        this.element.src = 'flowering-bush-emoji.png';
-        this.element.style.position = 'absolute';
-        this.element.style.left = x;
-        this.element.style.top = y;
-        setEmojiSize(this.element, 'flowering-bush');
+        this.element = createEmojiElement('flowering-bush');
+        if (this.element) {
+            this.element.style.left = x;
+            this.element.style.top = y;
+        } else {
+            console.error('Failed to create FloweringBush element');
+        }
     }
 
     addToPlayArea() {
         const playArea = document.getElementById('play-area');
-        playArea.appendChild(this.element);
-        this.spawnButterflies();
+        if (this.element) {
+            playArea.appendChild(this.element);
+            console.log('FloweringBush added to play area');
+            this.spawnButterflies();
+        }
     }
 
     spawnButterflies() {
         // Logic to spawn butterflies
+        console.log('Spawning butterflies');
     }
 }
