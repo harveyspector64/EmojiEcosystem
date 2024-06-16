b30d978cf73ce735f7c21b22f5d95756dc984359
harveyspector64 <154754949+harveyspector64@users.noreply.github.com>
2024-06-16 12:35:35 -0700

    Create emoji_constants.js

---
+++ emoji_constants.js
@@ -0,0 +1,16 @@
+// emoji_constants.js
+const EMOJI_SIZES = {
+    'flowering-bush': { width: '40px', height: '40px' },
+    'tree': { width: '80px', height: '80px' },
+    'butterfly': { width: '20px', height: '20px' },
+    'bird': { width: '30px', height: '30px' },
+    'worm': { width: '15px', height: '15px' }
+};
+
+function setEmojiSize(element, type) {
+    const size = EMOJI_SIZES[type];
+    if (size) {
+        element.style.width = size.width;
+        element.style.height = size.height;
+    }
+}
